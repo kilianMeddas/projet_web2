fefbd0163504447dba06528acb904ccdc686f9c6
kilianMeddas <129312620+kilianMeddas@users.noreply.github.com>
2024-12-16 22:38:45 +0100

    Add files via upload

---
+++ page_accueil.php
@@ -0,0 +1,28 @@
+<html>
+
+<head>
+    <title>Page d'accueil</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=person"/>
+    <link rel="stylesheet" href="menu_deroulant.css">
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li class="deroulant"><a href="#"><span class="material-symbols-outlined">person</span></a>
+            </li>
+            <li class="deroulant"><a href="#">Notes</a>
+            </li>
+            <li class="deroulant"><a href="#">Documents</a>
+            </li>
+            <li class="deroulant"><a href="#">Cours et devoirs</a>
+            </li>
+            <li class="deroulant"><a href="#">Contacter</a>
+            </li>
+        </ul>
+    </nav>
+
+
+</body>
+
+</html>
